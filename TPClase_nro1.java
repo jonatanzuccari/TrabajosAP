9698fbd80980725b409e8e8fb71ae98235104fdf
jonatanzuccari <127139917+jonatanzuccari@users.noreply.github.com>
2023-03-06 17:58:57 -0300

    Add files via upload

---
+++ TPClase_nro1.java
@@ -0,0 +1,67 @@
+package PrimerPaquete;
+
+public class TPClase_nro1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //Ejercicio 1 parte a
+		int nI=5, nF=14;
+
+         /*while (nI<=nF) {
+	     System.out.print("," + nI++);
+         }*/
+	
+
+         //Ejercicio 1 parte b
+
+	     /*while (nI<=nF) {
+	     if (nI%2 == 0) {
+	     System.out.print(nI);
+	     }
+	     nI++;
+}*/
+		//Ejercicio 1 parte c
+		
+		/*boolean pares=true;*/
+
+	    /* while (nI<nF) {
+	     if (pares == true ) {
+	    	 if (nI%2 == 0) {
+	     System.out.println(nI);
+	     }
+	     nI++;
+	     }
+	     else {
+	    	 if (nI%2!=0) {
+	    		 System.out.println (nI);
+	    		 }
+	    	 nI++;
+	     }
+	     }*/
+		
+		//Ejercicio 1 d
+		
+	    /* for (int i=14; i>=4 ; i--) {
+		  System.out.print("-" + i ); 
+	    }*/
+		
+		//Ejercicio 2
+		
+		//punto2
+				/*float ingresos = 489099;
+				int vehiculos = 2;
+				int inmuebles = 3;
+				boolean ingresosAltos = true;
+				
+				if ( (ingresos >= 489083) && ( vehiculos >= 3) &&(inmuebles >= 3)&& (ingresosAltos==true)) {
+				System.out.println("Categoria A");}
+		        if((ingresos >= 489083) || ( vehiculos >= 3) ||(inmuebles >= 3)|| (ingresosAltos==true)) {
+				System.out.println("Categoria B");}
+				if((ingresos < 489083) && ( vehiculos < 3) && (inmuebles <3) && (ingresosAltos!=true)) {
+				System.out.println("Categoria C");} */
+
+
+		
+	}
+
+}
